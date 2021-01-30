e3aa4e4381945a43744d978b8aa031b827915aa1
electro-333 <76483752+electro-333@users.noreply.github.com>
2021-01-30 22:55:19 +0100

    Create Download.py

---
+++ Download.py
@@ -0,0 +1,19 @@
+
+#pip install pafy
+#pip install youtube.dl
+
+
+from pafy import new
+import os
+while True:
+	link = input("enter link of video >> ")
+	video = new(link)
+	stream = video.streams
+	print("0 ",stream[0])
+	print("1 ",stream[1])
+	stre = int(input("enter quality "))
+	try:
+		stream[stre].download()
+	except:
+		print("there is a probleme with stream or link")
+		
